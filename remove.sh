a05c829991dcb906286e46095ccdaa03c74d7e8c
eifal <syahrifal7@gmail.com>
2024-06-22 23:50:27 +0700

    "Added removal of additional properties and modules from Android.bp files"

--- remove.sh
+++ remove.sh
@@ -2,5 +2,16 @@
 
 find . -name 'Android.bp' -exec sed -i '/default_team/d' {} +
 
-echo "Removed 'default_team' property from all Android.bp files"
+find . -name 'Android.bp' -exec sed -i '/product_variables.pdk/d' {} +
+
+find . -name 'Android.bp' -exec sed -i '/aconfig_declarations/d' {} +
+
+if [ -f "system/update_engine/libbinderwrapper/Android.bp" ]; then
+    sed -i '/module "libbinderwrapper_defaults"/,/}/d' system/update_engine/libbinderwrapper/Android.bp
+    sed -i '/module "libbinderwrapper"/,/}/d' system/update_engine/libbinderwrapper/Android.bp
+    sed -i '/module "libbinderwrapper_test_support"/,/}/d' system/update_engine/libbinderwrapper/Android.bp
+fi
+
+echo "Removed unrecognized properties and duplicate modules from Android.bp files"
+
 
