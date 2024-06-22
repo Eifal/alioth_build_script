c6e5942e4010e8317b42c80b868d47ecae56b649
eifal <syahrifal7@gmail.com>
2024-06-23 03:14:22 +0700

    add crdroid.sh

---
+++ crdroid.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Removals
+rm -rf .repo/local_manifests
+
+# Initialize repo with specified manifest
+repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs --depth=1
+
+# Clone local_manifests repository
+git clone https://github.com/Eifal/local_manifests --depth 1 .repo/local_manifests
+
+# Sync the repositories
+curl https://raw.githubusercontent.com/sounddrill31/docker-images/04449990912b9d7ee594193883740037f0ac80a7/aosp/common/resync.sh | bash
+
+#customs
+
+#build
+source build/envsetup.sh
+lunch crdroid_alioth-ap1a-userdebug
+mka bacon VERBOSE=1
+
+#sign build
