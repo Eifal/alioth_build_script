6d6320f15632367e87361e8c424ef8e548fd5f8a
eifal <syahrifal7@gmail.com>
2024-06-23 03:17:00 +0700

    "Remove lunch and mka commands, replace with brunch command"

--- crdroid.sh
+++ crdroid.sh
@@ -16,7 +16,6 @@ curl https://raw.githubusercontent.com/sounddrill31/docker-images/04449990912b9d
 
 #build
 source build/envsetup.sh
-lunch crdroid_alioth-ap1a-userdebug
-mka bacon VERBOSE=1
+brunch alioth VERBOSE=1
 
 #sign build
