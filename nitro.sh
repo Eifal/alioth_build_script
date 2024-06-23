0d230f365a1867f9bc2ff10589f00520cee6cba1
eifal <syahrifal7@gmail.com>
2024-06-24 04:27:43 +0700

    "Modified nitro.sh script: added removal of prebuilts/clang/host/linux-x86 and prebuilts/rust directories, changed build commands, and removed sign build section."

--- nitro.sh
+++ nitro.sh
@@ -9,14 +9,25 @@ repo init -u https://github.com/nitrogen-project/android_manifest.git -b 14 --de
 # Clone local_manifests repository
 git clone https://github.com/Eifal/local_manifests --depth 1 .repo/local_manifests
 
+# Remove something
+if [ -d "prebuilts/clang/host/linux-x86" ]; then
+    cd prebuilts/clang/host/linux-x86
+    git reset --hard HEAD
+    cd -
+fi
+
+if [ -d "prebuilts/rust" ]; then
+    cd prebuilts/rust
+    git reset --hard HEAD
+    cd -
+fi
+
 # Sync the repositories
 curl https://raw.githubusercontent.com/sounddrill31/docker-images/04449990912b9d7ee594193883740037f0ac80a7/aosp/common/resync.sh | bash
 
-#customs
-
-#build
+# Custom build commands
 source build/envsetup.sh
-lunch nitrogen_alioth-ap1a-userdebug
-make bacon VERBOSE=1
+breakfast alioth userdebug
 
-#sign build
+# Sign build (add your signing commands here)
+# sign_build_commands_here
