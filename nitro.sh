62fce6a2285fd23afbf00f76733297342c9f8336
eifal <syahrifal7@gmail.com>
2024-06-24 04:42:33 +0700

    "Update make command to 'make bacon VERBOSE=1' in nitro.sh"

--- nitro.sh
+++ nitro.sh
@@ -28,7 +28,7 @@ curl https://raw.githubusercontent.com/sounddrill31/docker-images/04449990912b9d
 # Custom build commands
 source build/envsetup.sh
 breakfast alioth userdebug
-make 
+make bacon VERBOSE=1
 
 # Sign build (add your signing commands here)
 # sign_build_commands_here
