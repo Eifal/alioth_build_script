19454a6a9d5fe31f5e47fac56d0cf71cda1e26b8
Eifvl 運命 <syahrifal7@gmail.com>
2024-06-25 13:09:28 +0700

    Update nitro.sh

--- nitro.sh
+++ nitro.sh
@@ -9,21 +9,13 @@ repo init -u https://github.com/nitrogen-project/android_manifest.git -b 14 --de
 # Clone local_manifests repository
 git clone https://github.com/Eifal/local_manifests --depth 1 .repo/local_manifests
 
-# Remove something
-if [ -d "prebuilts/clang/host/linux-x86" ]; then
-    rm -rf prebuilts/clang/host/linux-x86
-fi
-
-if [ -d "prebuilts/rust" ]; then
-    rm -rf prebuilts/rust
-fi
-
 # Sync the repositories
 curl https://raw.githubusercontent.com/sounddrill31/docker-images/04449990912b9d7ee594193883740037f0ac80a7/aosp/common/resync.sh | bash
 
 # Custom build commands
 source build/envsetup.sh
 lunch nitrogen_alioth-ap2a-userdebug
+make clean
 make bacon VERBOSE=1
 
 # Sign build (add your signing commands here)
