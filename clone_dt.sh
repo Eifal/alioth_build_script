44fc8493033fbe3ef77581420d20ce766e6d9a2a
Eifal <syahrifal7@gmail.com>
2024-08-03 03:30:16 +0700

    "Added git clone command for KernelSU repository"

--- clone_dt.sh
+++ clone_dt.sh
@@ -17,5 +17,6 @@ git clone --depth 1 --branch staging https://github.com/EmanuelCN/kernel_xiaomi_
 git clone --depth 1 --branch lineage-21 https://github.com/TheMuppets/proprietary_vendor_xiaomi_alioth vendor/xiaomi/alioth
 git clone --depth 1 --branch lineage-21 https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
 # git clone --depth 1 --branch aosp-14 https://gitlab.com/johnmart19/vendor_xiaomi_camera vendor/xiaomi/camera
+git clone --depth 1 --branch main https://github.com/tiann/KernelSU kernel/xiaomi/sm8250/KernelSU
 
 echo "Script completed."
