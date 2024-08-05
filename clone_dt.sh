12e836c752002f66776bbad6bc9c844bec032d7a
Eifal <syahrifal7@gmail.com>
2024-08-06 01:45:12 +0700

    "Uncommented rm and git clone commands for vendor/xiaomi/camera"

--- clone_dt.sh
+++ clone_dt.sh
@@ -7,7 +7,7 @@ rm -rf device/xiaomi/sm8250-common
 rm -rf kernel/xiaomi/sm8250
 rm -rf vendor/xiaomi/alioth
 rm -rf vendor/xiaomi/sm8250-common
-# rm -rf vendor/xiaomi/camera
+rm -rf vendor/xiaomi/camera
 
 # Clone repositories
 echo "Cloning repositories..."
@@ -16,7 +16,7 @@ git clone --depth 1 --branch lineage-21-alpha https://github.com/Eifal/android_d
 git clone --depth 1 --branch staging https://github.com/EmanuelCN/kernel_xiaomi_sm8250 kernel/xiaomi/sm8250
 git clone --depth 1 --branch lineage-21 https://github.com/TheMuppets/proprietary_vendor_xiaomi_alioth vendor/xiaomi/alioth
 git clone --depth 1 --branch lineage-21 https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
-# git clone --depth 1 --branch aosp-14 https://gitlab.com/johnmart19/vendor_xiaomi_camera vendor/xiaomi/camera
+git clone --depth 1 --branch aosp-14 https://gitlab.com/johnmart19/vendor_xiaomi_camera vendor/xiaomi/camera
 git clone --depth 1 --branch main https://github.com/tiann/KernelSU kernel/xiaomi/sm8250/KernelSU
 
 echo "Script completed."
