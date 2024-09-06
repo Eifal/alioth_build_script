a0358e6d06a609a1e04292e540ee2abc648fc81f
Eifal <syahrifal7@gmail.com>
2024-09-07 00:26:30 +0700

    update script for rising

--- rising.sh
+++ rising.sh
@@ -6,7 +6,15 @@ rm -rf .repo/local_manifests
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 
 # local manifests
-git clone https://github.com/Eifal/local_manifests -b rising .repo/local_manifests
+git clone https://github.com/eprjkt/local_manifests -b main .repo/local_manifests
+
+# Remove existing directories
+rm -rf device/xiaomi/alioth
+rm -rf device/xiaomi/sm8250-common
+rm -rf vendor/xiaomi/alioth
+rm -rf vendor/xiaomi/sm8250-common
+rm -rf kernel/xiaomi/sm8250
+rm -rf vendor/xiaomi/camera
 
 # Sync
 /opt/crave/resync.sh
