37961b863925a7bddb293207c9a7834c98c96eaa
Eifal <syahrifal7@gmail.com>
2024-09-09 02:10:20 +0700

    "Added kernel cloning and updated rm commands in rising.sh"

--- rising.sh
+++ rising.sh
@@ -16,6 +16,9 @@ rm -rf vendor/xiaomi/sm8250-common
 rm -rf kernel/xiaomi/sm8250
 rm -rf vendor/xiaomi/camera
 
+# clone kernel
+git clone --recurse-submodules -b magictime-new https://github.com/TIMISONG-dev/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250
+
 # Sync
 /opt/crave/resync.sh
 
